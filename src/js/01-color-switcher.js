60b8f20f2ecb9599c8f72fa94bd54d1b059c898a
OlyaTrush <olya.trush@gmail.com>
2022-08-11 14:30:44 +0300

    bug fixes

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -4,6 +4,8 @@ const refs = {
   bodyColor: document.querySelector('body'),
 };
 
+let timerId = null;
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
